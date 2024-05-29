bdd9d23e6fe5430bb08e0cdcc1ac15839f03acbc
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-05-29 11:53:29 +0100

    LoginButton changes

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,14 +1,23 @@
 import { signIn } from "@/auth";
 
 export function LoginButton() {
-  async function handleLogin() {
-    "use server";
-    await signIn();
-  }
+  // async function handleLogin() {
+  //   "use server";
+  //   await signIn();
+  // }
 
   return (
-    <form action={handleLogin} className="inline">
-      <button className="bg-pink-300 text-black px-3 py-2 rounded">
+    <form
+      action={async () => {
+        "use server";
+        await signIn();
+      }}
+      className="inline"
+    >
+      <button
+        className="bg-pink-300 text-black px-3 py-2 rounded"
+        type="submit"
+      >
         Login
       </button>
     </form>
