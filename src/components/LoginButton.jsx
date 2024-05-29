8e012d8944c746bfef54c28a6f6aea3cd207cacf
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-05-29 12:08:31 +0100

    tried client change for log in to fix 404

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,4 +1,6 @@
-import { signIn } from "@/auth";
+"use client";
+// import { signIn } from "@/auth.js";
+import { signIn } from "next-auth/react";
 
 export function LoginButton() {
   // async function handleLogin() {
@@ -7,19 +9,20 @@ export function LoginButton() {
   // }
 
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn();
-      }}
-      className="inline"
-    >
-      <button
-        className="bg-pink-300 text-black px-3 py-2 rounded"
-        type="submit"
-      >
-        Login
-      </button>
-    </form>
+    // <form
+    //   action={async () => {
+    //     "use server";
+    //     await signIn();
+    //   }}
+    //   className="inline"
+    // >
+    //   <button
+    //     className="bg-pink-300 text-black px-3 py-2 rounded"
+    //     type="submit"
+    //   >
+    //     Login
+    //   </button>
+    // </form>
+    <button onClick={() => signIn()}>Sign In</button>
   );
 }
